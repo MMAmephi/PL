aee478ca222deb3f88eb18c25dd703ede65956bd
MMAmephi <merkulovm12@gmail.com>
2023-12-17 18:15:14 +0300

    initial

--- 10-rectangle/main.cpp
+++ 10-rectangle/main.cpp
@@ -2,7 +2,7 @@
 #include "Rectangle.h"
 
 int main() {
-    Rectangle rect1(1, 4, 3, 2), rect2(2, 3, 4, 1);
+    Rectangle rect1(1, 1, 3, 2), rect2(2, 2, 3, 1);
     if (rect2.check_collision(rect1)) {
         cout << "True";
     }
